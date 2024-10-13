338ea1a72eeace9d2785896f7faa683d73e54d9c
Kyle Verwey <108106818+starzzots@users.noreply.github.com>
2024-10-13 23:58:57 +0000

    setup forms

--- app.rb
+++ app.rb
@@ -2,12 +2,21 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
 end
 
-get("/howdy") do
-  erb(:hello)
+get("/square/new") do
+  erb(:square)
+end
+
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/payment/new") do
+  erb(:payment)
+end
+
+get("/random/new") do
+  erb(:random)
 end
